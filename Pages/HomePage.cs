2d4f1dc8c4464cbd6c85bff763b0be5cda208f09
Arpik Sargsyan <arpiksargsyan@gmail.com>
2024-11-19 16:35:22 +0400

    Add HomePage and HomePageTest to POM

---
+++ Pages/HomePage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+
+namespace DemoQA.PageObjects
+{
+    public class HomePage
+    {
+        private readonly IWebDriver _driver;
+
+        // Constructor
+        public HomePage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        // Locators
+        private IWebElement ElementsSection => _driver.FindElement(By.XPath("//h5[text()='Elements']"));
+        private IWebElement FormsSection => _driver.FindElement(By.XPath("//h5[text()='Forms']"));
+
+        // Actions
+        public void GoToElementsSection()
+        {
+            ElementsSection.Click();
+        }
+
+        public void GoToFormsSection()
+        {
+            FormsSection.Click();
+        }
+    }
+}
---
+++ Tests/HomePageTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using DemoQA.PageObjects;
+using OpenQA.Selenium.Support.UI;
+
+namespace DemoQA.Tests
+{
+    public class HomePageTest
+    {
+        private IWebDriver _driver;
+        private HomePage _homePage;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl("https://demoqa.com/");
+            _homePage = new HomePage(_driver);
+        }
+
+        [Test]
+        public void TestGoToElementsSection()
+        {
+            IWebElement element = _driver.FindElement(By.XPath("//h5[text()='Elements']"));
+            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].scrollIntoView(true);", element);
+            element.Click();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _driver.Quit();
+        }
+    }
+}
+
