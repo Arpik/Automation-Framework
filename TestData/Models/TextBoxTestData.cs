d205c80bcb897d0169693d4ef1c38252f8b04d50
Arpik Sargsyan <arpiksargsyan@gmail.com>
2024-12-06 18:16:48 +0400

    Add TestData\JSON TestData\Models structure to separate test data

---
+++ TestData/Models/TextBoxTestData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourProject.TestData.Models
+{
+    public class TextBoxTestData
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public bool IsValid { get; set; }
+    }
+}
+
