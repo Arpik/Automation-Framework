2f6bbc90f55cd5ae4b66fe88a4e580fd03740ad7
Arpik Sargsyan <arpiksargsyan@gmail.com>
2024-12-12 19:16:42 +0400

    Update the TextBoxTest structure to read data from JSON

--- TestData/Models/TextBoxTestData.cs
+++ TestData/Models/TextBoxTestData.cs
@@ -10,6 +10,8 @@ namespace YourProject.TestData.Models
     {
         public string FullName { get; set; }
         public string Email { get; set; }
+        public string CurrentAddress { get; set; }
+        public string PermanentAddress {  get; set; }
         public bool IsValid { get; set; }
     }
 }
---
+++ Utilities/LoadTestDataFromJson.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YourProject.TestData.Models;
+
+namespace AutomationFramework.Utilities
+{
+    internal class LoadTestDataFromJson
+    {
+        public static class TestDataLoader
+        {
+            public static IEnumerable<TextBoxTestData> LoadTestDataFromJson(string fileName)
+            {
+                string filePath = Path.Combine("TestData", "JSON", fileName);
+                if (!File.Exists(filePath))
+                {
+                    throw new FileNotFoundException($"Test data file '{fileName}' not found.");
+                }
+
+                var jsonData = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<List<TextBoxTestData>>(jsonData);
+            }
+        }
+    }
+}
